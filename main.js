91ef633f1612d9b069bf07f57f3f13f4f23bcd26
Bashari Rivka <basrivka1@gmail.com>
2024-01-31 21:38:31 +0200

    Add size until 400px

--- main.js
+++ main.js
@@ -4,8 +4,13 @@ function onBallClick() {
     const elBall = document.querySelector('.ball')
     var size = +elBall.innerText + 50
     elBall.innerText = size
-  
     elBall.style.width = size + 'px'
     elBall.style.height = size + 'px'
 
+    if (size > 400) {
+        size = 100
+        elBall.innerText = size
+        elBall.style.width = '100px'
+        elBall.style.height = '100px'
+    }
 }

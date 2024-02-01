c1f66f0a92673a3f30a268d13b628d0c4821f4cf
Bashari Rivka <basrivka1@gmail.com>
2024-02-01 11:59:52 +0200

    Add function onBall1Click

--- js/main.js
+++ js/main.js
@@ -15,3 +15,18 @@ function onBallClick() {
     }
     elBall.style.background = getRandomColor()
 }
+
+function onBall1Click() {
+    const elBall = document.querySelector('.ball1')
+    var size = +elBall.innerText + 50
+    elBall.innerText = size
+    elBall.style.width = size + 'px'
+    elBall.style.height = size + 'px'
+    var maxDiameter = size
+    if (maxDiameter > 300) {
+        size = 100
+        elBall.innerText = size
+        elBall.style.width = '100px'
+        elBall.style.height = '100px'
+    }
+}
